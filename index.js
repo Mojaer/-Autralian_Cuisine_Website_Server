2709d9cf1e706199aa679fc19c83a83f118e1130
Mojaer Ahmed <mojaermohi@gmail.com>
2023-05-03 23:47:17 +0600

    cors is imported and used

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
-
+var cors = require('cors');
 const port = 4000;
 
-
+app.use(cors());
 
 const chefs = require('./Resources/Chefs-Australia.json')
 const recipes = require('./Resources/recipes.json')
