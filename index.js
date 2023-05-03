098d69214d43785be8b9bda031890bd52a2b4013
Mojaer Ahmed <mojaermohi@gmail.com>
2023-05-03 23:45:32 +0600

    chefs are sent from the server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const app = express()
+const app = express();
+
 const port = 4000;
+
+
 
 const chefs = require('./Resources/Chefs-Australia.json')
 const recipes = require('./Resources/recipes.json')
 
-app.get('/chefs', (req, res) => {
+app.get('/', (req, res) => {
     res.send(chefs)
 })
 
@@ -16,12 +19,7 @@ app.get('/chefs/:id', (req, res) => {
     res.send(selectedChef)
 })
 
-app.get('/chefs/recipe/:id', (req, res) => {
 
-    const id = req.params.id
-    const chefRecipes = recipes.filter((recipe) => recipe.chef_id == id)
-    res.send(chefRecipes)
-})
 
 app.listen(port, () => {
     console.log(`the app is running at http://localhost:${port}`)
